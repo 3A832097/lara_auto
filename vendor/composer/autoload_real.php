24b4b2c8178d53dd3e6438cc222d074d4fa9e66c
Hsin-Ju Teng <dlulu900821@gmail.com>
2021-10-16 20:22:45 +0800

    nsebot/carbon安裝

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,24 @@ class ComposerAutoloaderInit883ecda5009e70a1042499caff815880
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInit883ecda5009e70a1042499caff815880::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire883ecda5009e70a1042499caff815880($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequire883ecda5009e70a1042499caff815880($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
